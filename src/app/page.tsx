f4715b37dac37fb044f6dde3d4120d5d053e9cbc
rajpatel2435 <rajpatel2435@gmail.com>
2024-12-12 01:05:48 -0500

    feat: added refresh image & components in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
-
+    <Navbar />
+    <CatsInfo />
     </div>
   );
 }
