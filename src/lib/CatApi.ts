2897470bf703c1e3b3c5cdaf325831f507b9f475
rajpatel2435 <rajpatel2435@gmail.com>
2024-12-12 01:03:03 -0500

    fix: improve API response

--- src/lib/CatApi.ts
+++ src/lib/CatApi.ts
@@ -2,14 +2,17 @@ import axios, { AxiosResponse } from 'axios';
 
 const fetchCatData = async (): Promise<{ catImage: string; catFact: string } | null> => {
   try {
-    
-    // Cat fact API (ninja)
 
-    const response: AxiosResponse = await axios.get(`https://catfact.ninja/facts/?page=${Math.floor(Math.random() * 34)}`);
-    const catFact: string = response?.data?.data[Math.floor(Math.random() * 10)]?.fact;
+    // Start fetching parallel to improve performance
+    const [catFactResponse, catImageResponse] = await Promise.all([
+      axios.get(`https://catfact.ninja/facts/?page=${Math.floor(Math.random() * 34)}`),
+      axios.get(`https://api.thecatapi.com/v1/images/search`),
+    ]);
+
+    // facts
+    const catFact: string = catFactResponse?.data?.data[Math.floor(Math.random() * 10)]?.fact;
 
     // Image API
-    const catImageResponse: AxiosResponse = await axios.get(`https://api.thecatapi.com/v1/images/search`);
     const catImageURL = catImageResponse?.data[0]?.url;
 
     return {
