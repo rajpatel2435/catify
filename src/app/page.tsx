95b5e9fd2ff05a870261a4b0b095ead6114a6b5a
rajpatel2435 <rajpatel2435@gmail.com>
2024-12-12 00:32:42 -0500

    chore: project setup for catify

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import CatsInfo from "@/components/CatsInfo";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+
+    </div>
+  );
+}
